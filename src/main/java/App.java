e5e2554777b115a3c2b0deb25bf9ef14ec036a25
Elias Pehkonen <elias.pehkonen@metropolia.fi>
2024-03-01 13:33:34 +0200

    second commit / add function is added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!" + add(3, 232));
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,12 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class AppTest {
+    App app = new App();
+
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void testAdd() {
+        assertEquals(app.add(1, 1),2);
     }
 }
